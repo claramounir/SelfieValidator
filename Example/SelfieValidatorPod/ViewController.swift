2a13965426a0e422d604ca0f6aa203cb31fbdfb8
Clara Mounir Adly <claramounir555@gmail.com>
2023-07-09 18:16:28 +0300

    fix refrence

--- Example/SelfieValidatorPod/ViewController.swift
+++ Example/SelfieValidatorPod/ViewController.swift
@@ -7,17 +7,30 @@
 //
 
 import UIKit
-
+import SelfieValidatorPod
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    @IBAction func startButton(_ sender: UIButton) {
+        startButton.layer.cornerRadius = 8.0
+        startButton.layer.masksToBounds = true
+        let wireFrame = SelfieValidatorWireFrame()
+        let view = wireFrame.assembleModule(moduleDlegate: self)
+        self.navigationController!.pushViewController(view, animated: true)
+    }
+}
+
+extension ViewController: ImageCapturedDelegate {
+    func didCapturedImage(_image: UIImage) {
+        imageView.image = _image
     }
 
 }
--- SelfieValidatorPod.podspec
+++ SelfieValidatorPod.podspec
@@ -20,7 +20,7 @@ Pod::Spec.new do |s|
    s.screenshots     = 'www.example.com/screenshots_1', 'www.example.com/screenshots_2'
   s.license          = 'MIT'
   s.author           = { 'claramounir' => 'claramounir555@gmail.com' }
-  s.source           = { :git => 'https://github.com/claramounir/SelfieValidatorPod.git', :tag => s.version.to_s }
+  s.source           = { :git => 'https://github.com/claramounir/SelfieValidator.git', :tag => s.version.to_s }
   # s.social_media_url = 'https://twitter.com/<TWITTER_USERNAME>'
 
   s.ios.deployment_target = '11.0'
